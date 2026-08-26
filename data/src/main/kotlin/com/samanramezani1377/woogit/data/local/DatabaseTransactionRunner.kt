5b856a31b2cf69d453ab5abb0f82fa88299bbf03
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-26 07:42:06 +0330

    E3.02: add explicit transaction boundary

---
+++ data/src/main/kotlin/com/samanramezani1377/woogit/data/local/DatabaseTransactionRunner.kt
@@ -0,0 +1,7 @@
+package com.samanramezani1377.woogit.data.local
+
+import app.cash.sqldelight.db.SqlDriver
+
+class DatabaseTransactionRunner(private val driver: SqlDriver) {
+    fun <T> transaction(block: () -> T): T = driver.newTransaction().use { block() }
+}
