afb299cf01bf3d551054bb045e6277135616d3a1
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-26 08:53:38 +0330

    Register Kotlin serialization plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("org.jetbrains.kotlin.multiplatform") version "2.2.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20" apply false
     id("app.cash.sqldelight") version "2.1.0" apply false
 }
