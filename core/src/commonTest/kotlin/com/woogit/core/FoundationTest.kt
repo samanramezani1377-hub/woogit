927d270bfbcf149f34225bc5756aec5e38ae686a
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-26 07:24:26 +0330

    E0.06: add first shared core test

---
+++ core/src/commonTest/kotlin/com/woogit/core/FoundationTest.kt
@@ -0,0 +1,11 @@
+package com.woogit.core
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class FoundationTest {
+    @Test
+    fun foundationTestInfrastructureIsAvailable() {
+        assertTrue(true)
+    }
+}
