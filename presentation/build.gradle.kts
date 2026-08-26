86a1e35c4ea36b6b44d78fdee3bba1c7f7d3b7c1
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-26 07:27:11 +0330

    E0.02: make presentation module buildable boundary

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,2 +1,13 @@
-// E0.02 boundary placeholder.
-// Compose configuration is applied in E0.03.
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.samanramezani1377.woogit.presentation"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 26
+    }
+}
