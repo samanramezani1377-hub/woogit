3bce61feaba282c855ce811d24bd533c5eb4ade3
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-26 07:40:54 +0330

    E3.01: configure SQLDelight database tooling

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("org.jetbrains.kotlin.multiplatform") version "2.2.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
+    id("app.cash.sqldelight") version "2.1.0" apply false
 }
