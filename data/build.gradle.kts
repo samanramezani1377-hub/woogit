0806de91ef032e778be4ca592ea6df98cab38971
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-26 07:41:26 +0330

    E3: wire SQLDelight into data module

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,15 +1,23 @@
 plugins {
     id("com.android.library")
+    id("app.cash.sqldelight")
 }
 
 android {
     namespace = "com.samanramezani1377.woogit.data"
     compileSdk = 36
-    defaultConfig {
-        minSdk = 26
-    }
+    defaultConfig { minSdk = 26 }
 }
 
 dependencies {
     implementation(project(":core"))
+    implementation("app.cash.sqldelight:android-driver:2.1.0")
+}
+
+sqldelight {
+    databases {
+        create("WooGitDatabase") {
+            packageName.set("com.samanramezani1377.woogit.data.db")
+        }
+    }
 }
