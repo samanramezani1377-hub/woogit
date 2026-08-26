89a73c635e547160bf4f79277c99ea73dd6132c6
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-26 07:55:48 +0330

    E4.01: define secure credential storage boundary

---
+++ core/src/commonMain/kotlin/com/samanramezani1377/woogit/core/security/SecureCredentialStore.kt
@@ -0,0 +1,15 @@
+package com.samanramezani1377.woogit.core.security
+
+import com.samanramezani1377.woogit.core.domain.model.CredentialReference
+
+/** Credentials are transient secrets and must never be part of domain persistence. */
+interface SecureCredentialStore {
+    fun put(reference: CredentialReference, consumerKey: String, consumerSecret: String)
+    fun get(reference: CredentialReference): CredentialPair?
+    fun remove(reference: CredentialReference)
+}
+
+data class CredentialPair(
+    val consumerKey: String,
+    val consumerSecret: String
+)
