d825fbafb02ea2a9741573217485fb8ecd00726b
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-26 07:37:06 +0330

    E1.06: complete core use case contract surface

--- core/src/commonMain/kotlin/com/samanramezani1377/woogit/core/domain/usecase/UseCases.kt
+++ core/src/commonMain/kotlin/com/samanramezani1377/woogit/core/domain/usecase/UseCases.kt
@@ -5,6 +5,12 @@ import com.samanramezani1377.woogit.core.domain.entity.StoreId
 import com.samanramezani1377.woogit.core.domain.error.CoreResult
 
 interface GetOrder<T> { suspend operator fun invoke(storeId: StoreId, id: EntityId): CoreResult<T> }
+interface ListOrders<T> { suspend operator fun invoke(storeId: StoreId): CoreResult<List<T>> }
+interface UpdateOrder<T> { suspend operator fun invoke(storeId: StoreId, id: EntityId, value: T): CoreResult<T> }
 interface GetProduct<T> { suspend operator fun invoke(storeId: StoreId, id: EntityId): CoreResult<T> }
+interface ListProducts<T> { suspend operator fun invoke(storeId: StoreId): CoreResult<List<T>> }
+interface UpdateProduct<T> { suspend operator fun invoke(storeId: StoreId, id: EntityId, value: T): CoreResult<T> }
 interface ConnectStore { suspend operator fun invoke(storeId: StoreId): CoreResult<Unit> }
+interface DisconnectStore { suspend operator fun invoke(storeId: StoreId): CoreResult<Unit> }
 interface SyncStore { suspend operator fun invoke(storeId: StoreId): CoreResult<Unit> }
+interface RetryPendingOperation { suspend operator fun invoke(storeId: StoreId, operationId: EntityId): CoreResult<Unit> }
