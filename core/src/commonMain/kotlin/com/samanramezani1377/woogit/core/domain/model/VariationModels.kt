1fbb0ee36a9360f10288817d256429b619ff6c26
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-26 17:32:55 +0330

    Complete V1 variation domain version field

--- core/src/commonMain/kotlin/com/samanramezani1377/woogit/core/domain/model/VariationModels.kt
+++ core/src/commonMain/kotlin/com/samanramezani1377/woogit/core/domain/model/VariationModels.kt
@@ -1,6 +1,17 @@
 package com.samanramezani1377.woogit.core.domain.model
 
 import com.samanramezani1377.woogit.core.domain.entity.EntityId
+import kotlinx.datetime.Instant
 
-data class Variation(val id: EntityId, val productId: EntityId, val attributes: List<VariationAttribute>, val pricing: Pricing, val stock: Stock?, val sku: String?, val image: ProductImage?)
+data class Variation(
+    val id: EntityId,
+    val productId: EntityId,
+    val attributes: List<VariationAttribute>,
+    val pricing: Pricing,
+    val stock: Stock?,
+    val sku: String?,
+    val image: ProductImage?,
+    val modifiedAt: Instant? = null,
+)
+
 data class VariationAttribute(val name: String, val option: String)
