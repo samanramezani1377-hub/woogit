87434b1f65aaaf1c699ba3134dbf3e14c9a4f2e2
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-26 21:00:57 +0330

    fix(data): use public SQLDelight transaction API

--- data/src/main/kotlin/com/samanramezani1377/woogit/data/local/DatabaseTransactionRunner.kt
+++ data/src/main/kotlin/com/samanramezani1377/woogit/data/local/DatabaseTransactionRunner.kt
@@ -3,15 +3,5 @@ package com.samanramezani1377.woogit.data.local
 import app.cash.sqldelight.db.SqlDriver
 
 class DatabaseTransactionRunner(private val driver: SqlDriver) {
-    fun <T> transaction(block: () -> T): T {
-        val transaction = driver.newTransaction().value
-        return try {
-            val result = block()
-            transaction.endTransaction(successful = true)
-            result
-        } catch (error: Throwable) {
-            transaction.endTransaction(successful = false)
-            throw error
-        }
-    }
+    fun <T> transaction(block: () -> T): T = driver.executeInTransactionWithResult(block)
 }
