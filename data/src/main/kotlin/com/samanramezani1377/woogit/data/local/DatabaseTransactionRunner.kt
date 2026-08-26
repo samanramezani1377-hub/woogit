8e373bf7dd03f4623b2bf8c5b3a6f5b3e2040ff4
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-26 07:42:43 +0330

    E3: fix SQLDelight transaction lifecycle

--- data/src/main/kotlin/com/samanramezani1377/woogit/data/local/DatabaseTransactionRunner.kt
+++ data/src/main/kotlin/com/samanramezani1377/woogit/data/local/DatabaseTransactionRunner.kt
@@ -3,5 +3,15 @@ package com.samanramezani1377.woogit.data.local
 import app.cash.sqldelight.db.SqlDriver
 
 class DatabaseTransactionRunner(private val driver: SqlDriver) {
-    fun <T> transaction(block: () -> T): T = driver.newTransaction().use { block() }
+    fun <T> transaction(block: () -> T): T {
+        val transaction = driver.newTransaction().value
+        return try {
+            val result = block()
+            transaction.endTransaction(successful = true)
+            result
+        } catch (error: Throwable) {
+            transaction.endTransaction(successful = false)
+            throw error
+        }
+    }
 }
