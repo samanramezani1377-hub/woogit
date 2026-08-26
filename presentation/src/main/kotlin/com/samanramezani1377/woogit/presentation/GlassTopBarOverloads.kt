75fe43ca1799ee57971eff0c99fab62d87909344
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-26 17:49:25 +0330

    E11 fix GlassTopBar subtitle call compatibility

---
+++ presentation/src/main/kotlin/com/samanramezani1377/woogit/presentation/GlassTopBarOverloads.kt
@@ -0,0 +1,8 @@
+package com.samanramezani1377.woogit.presentation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun GlassTopBar(title: String, subtitle: String) {
+    GlassTopBar(title = title, subtitle = subtitle, modifier = androidx.compose.ui.Modifier)
+}
