8c3245c0bb230a9799589f204de9afafd7870dd4
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-26 07:22:28 +0330

    E0.03: configure core Kotlin Multiplatform module

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,2 +1,26 @@
-// E0.02 boundary placeholder.
-// Platform-independent KMP configuration is applied in E0.03.
+plugins {
+    id("org.jetbrains.kotlin.multiplatform")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+kotlin {
+    androidTarget()
+
+    sourceSets {
+        commonMain.dependencies {
+        }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+}
+
+android {
+    namespace = "com.samanramezani1377.woogit.core"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 26
+    }
+}
