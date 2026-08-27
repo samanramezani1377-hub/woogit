968ada8285b1f4aaa58a44533ad322fa7da2893c
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-27 16:01:33 +0330

    feat(products): add product category use case contract

--- core/src/commonMain/kotlin/com/samanramezani1377/woogit/core/domain/usecase/UseCases.kt
+++ core/src/commonMain/kotlin/com/samanramezani1377/woogit/core/domain/usecase/UseCases.kt
@@ -13,6 +13,7 @@ interface GetProducts { suspend operator fun invoke(storeId: StoreId, page: Int 
 interface CreateProduct { suspend operator fun invoke(storeId: StoreId, value: Product): CoreResult<Product> }
 interface UpdateProduct { suspend operator fun invoke(storeId: StoreId, id: EntityId, value: Product): CoreResult<Product> }
 interface DeleteProduct { suspend operator fun invoke(storeId: StoreId, id: EntityId): CoreResult<Unit> }
+interface GetProductCategories { suspend operator fun invoke(storeId: StoreId, page: Int = 1, perPage: Int = 100, search: String? = null): CoreResult<List<IdName>> }
 interface UploadMedia { suspend operator fun invoke(storeId: StoreId, fileName: String, bytes: ByteArray, mediaType: String): CoreResult<ProductImage> }
 interface DeleteMedia { suspend operator fun invoke(storeId: StoreId, mediaId: EntityId): CoreResult<Unit> }
 interface GetStore { suspend operator fun invoke(storeId: StoreId): CoreResult<StoreConnection> }
