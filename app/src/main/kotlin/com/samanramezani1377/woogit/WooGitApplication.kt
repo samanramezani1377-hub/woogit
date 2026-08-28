e4ce1cd41cfcff9cca9be832f0f62024fc3117a5
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-29 03:27:00 +0330

    feat(debug): capture uncaught application exceptions

--- app/src/main/kotlin/com/samanramezani1377/woogit/WooGitApplication.kt
+++ app/src/main/kotlin/com/samanramezani1377/woogit/WooGitApplication.kt
@@ -1,5 +1,28 @@
 package com.samanramezani1377.woogit
 
 import android.app.Application
+import com.samanramezani1377.woogit.debug.DebugConfig
+import com.samanramezani1377.woogit.debug.TechnicalErrorReporter
 
-class WooGitApplication:Application(){lateinit var composition:AppComposition;override fun onCreate(){super.onCreate();composition=AppComposition(this)}}
+class WooGitApplication : Application() {
+    lateinit var composition: AppComposition
+
+    override fun onCreate() {
+        super.onCreate()
+        if (DebugConfig.ENABLED) TechnicalErrorReporter.initialize(this)
+        composition = AppComposition(this)
+        if (DebugConfig.ENABLED) {
+            val previous = Thread.getDefaultUncaughtExceptionHandler()
+            Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+                TechnicalErrorReporter.report(
+                    feature = "Application",
+                    location = "WooGitApplication.onCreate / UncaughtExceptionHandler",
+                    operation = "Unhandled application exception",
+                    throwable = throwable,
+                    details = "thread=${thread.name}; previousHandler=${previous?.javaClass?.name.orEmpty()}",
+                )
+                previous?.uncaughtException(thread, throwable)
+            }
+        }
+    }
+}
