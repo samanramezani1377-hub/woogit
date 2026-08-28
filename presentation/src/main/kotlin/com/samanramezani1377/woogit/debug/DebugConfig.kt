52ae1633b024bef4a977c6921b17f0a2c5b062c7
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-29 03:16:10 +0330

    feat(debug): add removable debug diagnostics switch

---
+++ presentation/src/main/kotlin/com/samanramezani1377/woogit/debug/DebugConfig.kt
@@ -0,0 +1,6 @@
+package com.samanramezani1377.woogit.debug
+
+/** Temporary developer diagnostics switch. Set to false before release to hide debug UI. */
+internal object DebugConfig {
+    const val ENABLED = true
+}
