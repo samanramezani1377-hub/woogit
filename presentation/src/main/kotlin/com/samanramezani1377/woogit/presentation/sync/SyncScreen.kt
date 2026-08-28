0608550e1dbfe11ee684c38f6c806bccc4b06e46
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-28 20:56:45 +0330

    fix(v1): remove unused sync screen import

--- presentation/src/main/kotlin/com/samanramezani1377/woogit/presentation/sync/SyncScreen.kt
+++ presentation/src/main/kotlin/com/samanramezani1377/woogit/presentation/sync/SyncScreen.kt
@@ -3,7 +3,6 @@ package com.samanramezani1377.woogit.presentation.sync
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
