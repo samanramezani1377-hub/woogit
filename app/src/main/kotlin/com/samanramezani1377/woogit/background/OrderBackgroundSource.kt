c91e8b427022758735828afb2d06df8cfd69b51b
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-29 06:01:29 +0330

    feat: include order currency in notification data

--- app/src/main/kotlin/com/samanramezani1377/woogit/background/OrderBackgroundSource.kt
+++ app/src/main/kotlin/com/samanramezani1377/woogit/background/OrderBackgroundSource.kt
@@ -1,4 +1,14 @@
 package com.samanramezani1377.woogit.background
 
-interface OrderBackgroundSource{suspend fun findNewOrders(storeId:String):List<BackgroundOrder>}
-data class BackgroundOrder(val storeId:String,val orderId:Long,val number:String,val total:String,val itemSummary:String,val occurredAtEpochMillis:Long,val serverState:String)
+interface OrderBackgroundSource { suspend fun findNewOrders(storeId: String): List<BackgroundOrder> }
+
+data class BackgroundOrder(
+    val storeId: String,
+    val orderId: Long,
+    val number: String,
+    val total: String,
+    val currency: String,
+    val itemSummary: String,
+    val occurredAtEpochMillis: Long,
+    val serverState: String,
+)
