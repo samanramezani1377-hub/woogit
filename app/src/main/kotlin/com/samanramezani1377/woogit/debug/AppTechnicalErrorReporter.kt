7748bbac85aed4c50df6e2d7a35c03cf44dc4476
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-29 03:32:07 +0330

    feat(debug): bridge core technical errors to debug store

---
+++ app/src/main/kotlin/com/samanramezani1377/woogit/debug/AppTechnicalErrorReporter.kt
@@ -0,0 +1,26 @@
+package com.samanramezani1377.woogit.debug
+
+import android.content.Context
+import com.samanramezani1377.woogit.core.debug.TechnicalErrorContext
+import com.samanramezani1377.woogit.core.debug.TechnicalErrorReporter
+import com.samanramezani1377.woogit.presentation.debug.DebugLogStore
+
+class AppTechnicalErrorReporter(private val context: Context) : TechnicalErrorReporter {
+    override fun report(contextInfo: TechnicalErrorContext, throwable: Throwable?) {
+        DebugLogStore.append(
+            context = context,
+            feature = contextInfo.feature,
+            location = contextInfo.location,
+            type = contextInfo.type,
+            operation = contextInfo.operation,
+            userMessage = "",
+            throwable = throwable,
+            technicalMessage = contextInfo.details.ifBlank { throwable?.message.orEmpty() },
+            httpMethod = contextInfo.httpMethod,
+            endpoint = contextInfo.endpoint,
+            httpStatus = contextInfo.httpStatus,
+            responseBody = contextInfo.responseBody,
+            details = contextInfo.details,
+        )
+    }
+}
