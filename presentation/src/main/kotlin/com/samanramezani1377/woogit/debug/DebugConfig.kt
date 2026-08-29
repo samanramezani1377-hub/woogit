25bcb6966a16b4833219960fb88ab818e7c5d864
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-29 03:57:26 +0330

    fix(ci): expose debug config to app module

--- presentation/src/main/kotlin/com/samanramezani1377/woogit/debug/DebugConfig.kt
+++ presentation/src/main/kotlin/com/samanramezani1377/woogit/debug/DebugConfig.kt
@@ -1,6 +1,6 @@
 package com.samanramezani1377.woogit.debug
 
 /** Temporary developer diagnostics switch. Set to false before release to hide debug UI. */
-internal object DebugConfig {
+object DebugConfig {
     const val ENABLED = true
 }
