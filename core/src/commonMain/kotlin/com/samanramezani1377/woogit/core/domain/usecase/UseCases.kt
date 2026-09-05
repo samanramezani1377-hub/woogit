a1f4b2bee7129b0879d4839b69045a847f586d89
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-09-06 00:29:45 +0330

    Add collection count use cases

--- core/src/commonMain/kotlin/com/samanramezani1377/woogit/core/domain/usecase/UseCases.kt
+++ core/src/commonMain/kotlin/com/samanramezani1377/woogit/core/domain/usecase/UseCases.kt
@@ -7,10 +7,12 @@ import com.samanramezani1377.woogit.core.domain.model.*
 
 interface GetOrder { suspend operator fun invoke(storeId: StoreId, id: EntityId): CoreResult<Order> }
 interface GetOrders { suspend operator fun invoke(storeId: StoreId, page: Int = 1, perPage: Int = 20, search: String? = null, status: String? = null): CoreResult<List<Order>> }
+interface GetOrdersCount { suspend operator fun invoke(storeId: StoreId, search: String? = null, status: String? = null): CoreResult<Int> }
 interface GetSalesSummary { suspend operator fun invoke(storeId: StoreId): CoreResult<SalesSummary> }
 interface UpdateOrder { suspend operator fun invoke(storeId: StoreId, id: EntityId, value: Order): CoreResult<Order> }
 interface GetProduct { suspend operator fun invoke(storeId: StoreId, id: EntityId): CoreResult<Product> }
 interface GetProducts { suspend operator fun invoke(storeId: StoreId, page: Int = 1, perPage: Int = 20, search: String? = null): CoreResult<List<Product>> }
+interface GetProductsCount { suspend operator fun invoke(storeId: StoreId, search: String? = null): CoreResult<Int> }
 interface CreateProduct { suspend operator fun invoke(storeId: StoreId, value: Product): CoreResult<Product> }
 interface UpdateProduct { suspend operator fun invoke(storeId: StoreId, id: EntityId, value: Product): CoreResult<Product> }
 interface DeleteProduct { suspend operator fun invoke(storeId: StoreId, id: EntityId): CoreResult<Unit> }
