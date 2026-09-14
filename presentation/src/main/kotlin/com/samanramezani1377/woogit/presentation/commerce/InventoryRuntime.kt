2f8bc3c3fb0104130102885bc511b3e6b4ff8e37
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-09-15 00:18:07 +0330

    feat(inventory): add runtime bridge for quick stock updates

---
+++ presentation/src/main/kotlin/com/samanramezani1377/woogit/presentation/commerce/InventoryRuntime.kt
@@ -0,0 +1,9 @@
+package com.samanramezani1377.woogit.presentation.commerce
+
+import com.samanramezani1377.woogit.core.domain.entity.StoreId
+import com.samanramezani1377.woogit.core.domain.error.CoreResult
+import com.samanramezani1377.woogit.core.domain.model.Product
+
+internal object InventoryRuntime {
+    var updateProduct: (suspend (StoreId, Product) -> CoreResult<Product>)? = null
+}
