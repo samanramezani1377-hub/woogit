e3d6f59fd5fc1306e67e867c74b17b1e4b671fb4
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-09-20 15:33:24 +0330

    Use ComponentActivity for Bazaar billing registry

--- app/src/bazaar/kotlin/com/samanramezani1377/woogit/billing/BillingProviderInstaller.kt
+++ app/src/bazaar/kotlin/com/samanramezani1377/woogit/billing/BillingProviderInstaller.kt
@@ -1,6 +1,6 @@
 package com.samanramezani1377.woogit.billing
 
-import android.app.Activity
+import androidx.activity.ComponentActivity
 import com.samanramezani1377.woogit.bazaar.BazaarBillingClient
 import com.samanramezani1377.woogit.bazaar.BazaarBillingRuntime
 import com.samanramezani1377.woogit.core.billing.BillingGateway
@@ -8,12 +8,14 @@ import com.samanramezani1377.woogit.presentation.settings.BillingPurchaseRuntime
 
 object BillingProviderInstaller {
     private var client: BazaarBillingClient? = null
-    fun install(activity: Activity, gateway: BillingGateway) {
+
+    fun install(activity: ComponentActivity, gateway: BillingGateway) {
         BazaarBillingRuntime.registry = activity.activityResultRegistry
         client = BazaarBillingClient(activity.applicationContext, gateway)
         BillingPurchaseRuntime.purchase = { storeId, productId -> client!!.purchase(storeId, productId) }
         BillingPurchaseRuntime.reconcile = { storeId -> client!!.reconcile(storeId) }
     }
+
     fun clear() {
         BillingPurchaseRuntime.purchase = null
         BillingPurchaseRuntime.reconcile = null
